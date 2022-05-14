271b0c937d5a5ccab449c81022cf932644296976
Michael Gene Brockus <55331536+michael-gene-brockus@users.noreply.github.com>
2022-05-13 18:51:59 -0700

    Create main.cpp

---
+++ notebook/prog/main.cpp
@@ -0,0 +1,18 @@
+/*
+ *
+ * Troglobyte Learning:
+ * Teacher: Michael Gene Brockus
+ * Gmail: <mail: michaelbrockus@gmail.com>
+ *
+ */
+#include <iostream>
+
+enum { SUCCESS = 0 };
+
+//
+// main is where all programs start
+int main()
+{
+    std::cout << "Hello, World" << std::endl;
+    return SUCCESS;
+} // end of func main
